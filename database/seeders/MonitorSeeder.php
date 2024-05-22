432e5fa4705eeffa5b73d27c993ad4b80562cd50
David Joos <drdavejoos@gmail.com>
2024-05-22 08:34:48 +0200

    Update seeder and factory regarding new enums

--- database/seeders/MonitorSeeder.php
+++ database/seeders/MonitorSeeder.php
@@ -2,9 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Enums\ComparisonOperator;
+use App\Enums\TriggerType;
 use App\Models\Monitor;
+use App\Models\Trigger;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpFoundation\Response;
 
 class MonitorSeeder extends Seeder
 {
@@ -13,13 +18,32 @@ class MonitorSeeder extends Seeder
      */
     public function run(): void
     {
-        Monitor::factory(10)->create();
+        Monitor::factory(10)
+            ->for(User::factory())
+            ->has(
+                Trigger::factory()
+                    ->state([
+                        'type' => TriggerType::HTTP_STATUS_CODE,
+                        'value' => 500,
+                        'comparison_operator' => ComparisonOperator::GREATER_THAN,
+                    ])
+                    ->create()
+            );
     }
 
     public static function runFor(User $user, int $count = 1): void
     {
-        Monitor::factory($count)->create([
-            'user_id' => $user->id,
-        ]);
+        // With one trigger that says: if the HTTP status code is not the expected one, trigger an alert
+        Monitor::factory($count)
+            ->for($user)
+            ->has(
+                Trigger::factory()
+                    ->state([
+                        'type' => TriggerType::HTTP_STATUS_CODE,
+                        'value' => Response::HTTP_OK,
+                        'comparison_operator' => ComparisonOperator::NOT_EQUALS,
+                    ])
+                    ->create()
+            );
     }
 }
