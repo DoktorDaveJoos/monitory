e6c42004dced8afb5a77f793f6ecb51213045340
David Joos <drdavejoos@gmail.com>
2024-05-27 16:20:25 +0200

    Finalized atomic components

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -10,6 +10,17 @@ import {
     CardTitle,
 } from '@/Components/ui/card';
 import { Button } from '@/Components/ui/button';
+import { Input } from '@/Components/ui/input';
+import { Label } from '@/Components/ui/label';
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from '@/Components/ui/select';
 
 const handle = () => {
     console.log('Primary Button Clicked');
@@ -29,12 +40,57 @@ const handle = () => {
         </template>
 
         <div class="py-12">
-            <Card class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <Card class="max-w-6xl mx-auto">
                 <CardHeader>
                     <CardTitle>Card Title</CardTitle>
                     <CardDescription>Card Description</CardDescription>
                 </CardHeader>
-                <CardContent> Card Content</CardContent>
+                <CardContent>
+                    Some Card Content
+
+                    <div class="flex space-x-4">
+                        <div
+                            class="grid w-full max-w-sm items-center gap-1.5 my-6"
+                        >
+                            <Label for="email">Email</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                placeholder="Email"
+                            />
+                        </div>
+                        <div
+                            class="grid w-full max-w-sm items-center gap-1.5 my-6"
+                        >
+                            <Label for="type">Type</Label>
+                            <Select id="type">
+                                <SelectTrigger class="w-[180px]">
+                                    <SelectValue placeholder="Select a fruit" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        <SelectLabel>Fruits</SelectLabel>
+                                        <SelectItem value="apple">
+                                            Apple
+                                        </SelectItem>
+                                        <SelectItem value="banana">
+                                            Banana
+                                        </SelectItem>
+                                        <SelectItem value="blueberry">
+                                            Blueberry
+                                        </SelectItem>
+                                        <SelectItem value="grapes">
+                                            Grapes
+                                        </SelectItem>
+                                        <SelectItem value="pineapple">
+                                            Pineapple
+                                        </SelectItem>
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                    </div>
+                </CardContent>
                 <CardFooter class="space-x-4">
                     <Button @click="handle">Primary</Button>
                     <Button variant="secondary">Secondary</Button>
