becf0544fe7822e33bc428638633b4ef040ae69f
David Joos <drdavejoos@gmail.com>
2024-05-30 21:49:40 +0200

    add stats card

---
+++ resources/js/Components/Stats.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Card } from '@/Components/ui/card';
+import { Label } from '@/Components/ui/label';
+
+defineProps<{
+    label: string;
+    value: string;
+}>();
+</script>
+
+<template>
+    <Card class="flex flex-col justify-between px-4 py-4">
+        <div class="w-full text-center">
+            <Label class="mb-4">{{ label }}</Label>
+        </div>
+
+        <div class="w-full text-center">
+            <span class="font-mono text-lg">{{ value }}</span>
+        </div>
+    </Card>
+</template>
+
+<style scoped></style>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -6,6 +6,7 @@ import LayoutHeader from '@/Components/LayoutHeader.vue';
 import DashboardChart from '@/Components/DashboardChart.vue';
 import { Monitor, ResourceCollection } from '@/types';
 import { Label } from '@/Components/ui/label';
+import Stats from '@/Components/Stats.vue';
 
 defineProps<{
     monitors: ResourceCollection<Monitor>;
@@ -20,6 +21,15 @@ defineProps<{
         <template #header>
             <LayoutHeader :icon="Home" label="Dashboard" />
         </template>
+
+        <div class="grid grid-cols-12 gap-6 mb-10">
+            <div></div>
+            <Stats class="col-span-2" label="total checks" value="3.523.326" />
+            <Stats class="col-span-2" label="total notifications" value="251" />
+            <Stats class="col-span-2" label="total monitors" value="1" />
+            <Stats class="col-span-2" label="uptime overall" value="99.45%" />
+            <Stats class="col-span-2" label="avg response time" value="186ms" />
+        </div>
 
         <Label class="mb-4">Https / Http</Label>
         <div class="pb-12 space-y-6">
