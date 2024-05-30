8a00e8c364a1fa2b3e3097875ad2edfb77b22177
David Joos <drdavejoos@gmail.com>
2024-05-30 21:20:46 +0200

    Add group label

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -5,6 +5,7 @@ import { Home } from 'lucide-vue-next';
 import LayoutHeader from '@/Components/LayoutHeader.vue';
 import DashboardChart from '@/Components/DashboardChart.vue';
 import { Monitor, ResourceCollection } from '@/types';
+import { Label } from '@/Components/ui/label';
 
 defineProps<{
     monitors: ResourceCollection<Monitor>;
@@ -20,6 +21,7 @@ defineProps<{
             <LayoutHeader :icon="Home" label="Dashboard" />
         </template>
 
+        <Label class="mb-4">Https / Http</Label>
         <div class="pb-12 space-y-6">
             <DashboardChart
                 v-for="monitor in monitors.data"
